1d44065f6c98f8e95e867b82a2027c556ef1ebea
O Hedrick <ryebread8403@gmail.com>
2021-05-12 18:23:40 -0500

    now importing classes as one string instead of array
    Add-Type doesn't accept a string array, so
    get-content needs the raw parameter added.

--- PSGraphDB.psm1
+++ PSGraphDB.psm1
@@ -1,3 +1,3 @@
 $ClassDefinitionFile = join-path $PSScriptRoot PSGraphDB-Classes
-$ClassDefinitionText = get-content $ClassDefinitionFile
+$ClassDefinitionText = get-content $ClassDefinitionFile -Raw
 add-type -TypeDefinition $ClassDefinitionText
