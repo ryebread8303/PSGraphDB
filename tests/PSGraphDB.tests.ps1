fec6144e1518d498b88e1847720c9fd369a80852
O Hedrick <ryebread8403@gmail.com>
2021-05-15 14:57:05 -0500

    Changed IDs from int to Guid
    I won't need to track the current ID or increment it. Hopefully
    I won't need to be able to predict the next ID. This change
    shouldn't affect using GraphML for the file format.

--- tests/PSGraphDB.tests.ps1
+++ tests/PSGraphDB.tests.ps1
@@ -24,17 +24,15 @@ Describe "Graph Classes" {
         It "GraphNode should be a type." {
             [GraphNode] | Should Not Be $null
         }
-        It "Should have a working constructor for signature (int id, string[] lables)." {
-            $node = [GraphNode]::new(0,@("Device","Networking"))
-            $node.ID | Should Be 0
+        It "Should have a working constructor for signature (string[] lables)." {
+            $node = [GraphNode]::new(@("Device","Networking"))
             $node.Lables | Should Be @("Device","Networking")
         }
-        It "Should have a working constructor for signature (int id, string[] lables, Dictionary<string,string> properties)" {
+        It "Should have a working constructor for signature (string[] lables, Dictionary<string,string> properties)" {
             $props = new-object System.Collections.Generic.Dictionary"[String, String]"
             $props.Add("name","boopsie")
             $props.Add("color","red")
-            $node = [GraphNode]::new(0,@("Device","Networking"),$props)
-            $node.ID | Should Be 0
+            $node = [GraphNode]::new(@("Device","Networking"),$props)
             $node.Lables | Should Be @("Device","Networking")
             $node.Properties["name"] | Should Be "boopsie"
         }
@@ -43,14 +41,14 @@ Describe "Graph Classes" {
         It "GraphRelation should be a type." {
             [GraphRelation] | Should Not Be $null
         }
-        It "Should have a working constructor for signature (int id, GraphNode leftnode, string relationshiptype, GraphNode rightnode)" {
+        It "Should have a working constructor for signature (GraphNode leftnode, string relationshiptype, GraphNode rightnode)" {
             $BobProps = New-Object System.Collections.Generic.Dictionary"[String,String]"
             $BobProps.Add("name","Bob")
             $AliceProps = New-Object System.Collections.Generic.Dictionary"[String,String]"
             $AliceProps.Add("name","Alice")
-            $bob = [GraphNode]::new(0,"Person",$BobProps)
-            $alice = [GraphNode]::new(1,"Person",$AliceProps)
-            $relation = [GraphRelation]::new(0,$bob,"FRIENDS_WITH",$alice)
+            $bob = [GraphNode]::new("Person",$BobProps)
+            $alice = [GraphNode]::new("Person",$AliceProps)
+            $relation = [GraphRelation]::new($bob,"FRIENDS_WITH",$alice)
             $relation.LeftNode.Properties["name"] | Should Be "Bob"
             $relation.RightNode.Properties["name"] | Should Be "Alice"
             $relation.RelationshipType | Should Be "FRIENDS_WITH"
